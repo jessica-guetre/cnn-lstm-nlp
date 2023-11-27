396585a2dd18aa45b5da66dadb7f23e60093e224
AndyCraig200 <andycraig200@gmail.com>
2023-11-26 21:59:13 -0500

    modularization, merging into 1 single file.

---
+++ util/embeddings.py
@@ -0,0 +1,22 @@
+import numpy as np
+from util.constants import PATH_TO_GLOVE_FILE
+import gensim.downloader as api
+
+
+def load_glove():
+    embeddings_index = {}
+    with open(PATH_TO_GLOVE_FILE) as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, "f", sep=" ")
+            embeddings_index[word] = coefs
+
+    print("Found %s word vectors." % len(embeddings_index))
+    return embeddings_index
+
+
+def load_word2vec_pretrained():
+    # Load in embeddings
+    embeddings = api.load("word2vec-google-news-300")
+    return embeddings
+
